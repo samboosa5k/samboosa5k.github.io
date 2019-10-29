a9c99b1dec6637f43afe67b2992113421a017dfe
samboosa5k <43879542+samboosa5k@users.noreply.github.com>
2019-10-29 07:43:47 +0100

    upload of react developer website

---
+++ precache-manifest.4df298f083f766869d7422efdc658928.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75ce4ea7964fd11bf11e0021b194a69f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02ca541db06db23779dc",
+    "url": "/static/css/main.ba6fd569.chunk.css"
+  },
+  {
+    "revision": "742a99b2076b30dc2be9",
+    "url": "/static/js/2.7d5d4529.chunk.js"
+  },
+  {
+    "revision": "02ca541db06db23779dc",
+    "url": "/static/js/main.405c67e2.chunk.js"
+  },
+  {
+    "revision": "107b25d2ff5f9f869482",
+    "url": "/static/js/runtime-main.98867d41.js"
+  }
+]);
