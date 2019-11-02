ce856a1f40f56103aedeaf1d0adc8a85c3ac4f94
samboosa5k <43879542+samboosa5k@users.noreply.github.com>
2019-11-02 17:58:44 +0100

    added styles to the page, other than that some minor fixes to get rid of react error messages

---
+++ precache-manifest.657d18165377bc2d67dcd7c46b42a55a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a61be8c3a0adddfef4fabe4f4bb356",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb59ccb0ec6ac9cf8311",
+    "url": "/static/css/main.dd146dfe.chunk.css"
+  },
+  {
+    "revision": "742a99b2076b30dc2be9",
+    "url": "/static/js/2.7d5d4529.chunk.js"
+  },
+  {
+    "revision": "fb59ccb0ec6ac9cf8311",
+    "url": "/static/js/main.2f60c4ab.chunk.js"
+  },
+  {
+    "revision": "107b25d2ff5f9f869482",
+    "url": "/static/js/runtime-main.98867d41.js"
+  }
+]);
